acf3e87c0fcf88352baf499fd3a103078af00e89
Andrei Kopyltsov <andrushke@gmail.com>
2021-08-11 23:00:48 +0600

    Terraform1 done

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+provider "yandex" {
+  version                  = "0.35"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+  service_account_key_file = var.service_account_key_file
+}
+
+resource "yandex_compute_instance" "app" {
+  name = "reddit-app"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 5
+    gpus          = 0
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+    agent       = false
+    timeout     = "1m"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+
+  }
+
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable region {
+  description = "Region"
+  default     = "ru-central1"
+}
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable service_account_key_file {
+  description = "key .json"
+}
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+variable counter {
+  description = "Number of instances"
+  default     = 1
+}
