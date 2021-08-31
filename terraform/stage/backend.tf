75fff6cff6e0879a52c542b836a39ce31f8d941e
Andrei Kopyltsov <andrushke@gmail.com>
2021-08-31 12:28:44 +0600

    fixing last test

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -4,8 +4,8 @@ terraform {
     bucket                      = "epicbucket1"
     region                      = "ru-central1-a"
     key                         = "terraform.tfstate"
-    access_key                  = "access key"
-    secret_key                  = "secret key"
+    access_key                  = "accesskey"
+    secret_key                  = "secretkey"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
